c0f25d191c5ebc571e985f7d892f9ee3a37a4aec
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-12-15 12:33:37 +0530

    Create rock_paper_scissor_2_players.py

---
+++ main/Rock Paper Scissor/rock_paper_scissor_2_players.py
@@ -0,0 +1,69 @@
+s1=0
+s2=0
+
+# LOGICAL FUNCTION
+def winner(p1, p2):
+   
+   global s1
+   global s2
+   
+   if (p1 == p2):
+       s1+=1
+       s2+=1
+       return "It's a tie!"
+       
+   elif (p1 == 'r'):
+       if (p2 == 's)':
+           s1+=1
+           return "First Player wins!"
+       else:
+           s2+=1
+           return "Second Player wins!"
+           
+   elif (p1 == 's'):
+       if (p2 == 'p'):
+           s1+=1
+           return "First Player wins!"
+       else:
+           s2+=1
+           return"Second Player wins!"
+           
+   elif (p1 == 'p'):
+       if (p2 == 'r'):
+           s1+=1
+           return "First Player wins!"
+       else:
+           s2+=1
+           return "Second Player wins!"
+   else:
+       return "Invalid Input!"
+
+print(" --- ** --- !WELCOME TO THE R.P.S GAME! --- ** --- ")
+try:
+    c=int(input("\nHow many rounds will you like to play? Enter that number:"))
+except:
+    print("INVALID INPUT! Try again, next time!")
+    exit()
+
+if c<=0:
+    print("Negative number not allowed! Redirecting to 1 round only!")
+    c=1
+
+# DRIVER CODE...
+while(c>0):
+    print("\nRounds left -->",c)
+    player1 = input("\nFirst player: rock [enter 'r'], paper [enter 'p'] or scissors [enter 's']: ").lower()
+    player2 = input("Second Player: rock [enter 'r'], paper [enter 'p'] or scissors [enter 's']: ").lower()
+    print(winner(player1, player2))
+    c-=1
+
+# RESULTS
+if s1>s2:
+    print("\nUltimate Winner ---> First Player")
+elif s1<s2:
+    print("\nUltimate Winner ---> Second Player")
+else:
+    print("\nTie GAME!")
+    
+    
+@ CODED BY TSG405, 2020    
